add138654e81912b28b44f8e94572019ca7bc4d1
Luc Mosser <luc.mosser86@orange.fr>
2020-05-29 10:50:56 +0200

    this one works

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,30 +3,33 @@ import * as THREE from 'three'
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 import { Canvas, extend, useThree, useFrame } from "react-three-fiber";
 import { useSpring, a  } from "react-spring/three";
-import { OrbitControls, HTML, Text, Sky } from 'drei'
+import { HTML, Text, Sky } from 'drei';
 
 import './style.css'
 
+import Navbar from '../components/2dcomponents/Navbar/navbar';
 import Box from '../components/3dcomponents/box';
 import Plane from '../components/3dcomponents/plane';
 import Guitar from '../components/3dcomponents/guitar';
 import SpaceShip from '../components/3dcomponents/spaceship';
 
 
-// import Controls from '../components/controls';
+import Controls from '../components/controls';
 
 // import Header from '../components/2dcomponents/Header/header';
 
 
 export default () => (
   <div>
+  <Navbar />
   <Canvas camera={{ position: [0, 0, 5]}} onCreated={({ gl })=> {
     gl.shadowMap.enabled = true
     gl.shadowMap.type = THREE.PCFSoftShadowMap
   }}>
     <fog attach="fog" args={["white", 15, 25]}/>
-    <OrbitControls />
-    <Box position={[-1.2, 0, 0]} />
+    <Controls
+      autorotate={true}
+    />
     <SpaceShip />
     {/* <Plane /> */}
     {/* <Guitar /> */}
